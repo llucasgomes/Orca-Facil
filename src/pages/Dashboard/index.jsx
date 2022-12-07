0e2a4403106678bb7eea68567005d13f08deb972
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-12-06 21:38:53 -0400

    :lipstick: feat: Estilizaçao CSS do Grid

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,43 @@
+import CardOrcamento from "../../shared/components/CardOrcamento";
 import { Footer } from "../../shared/components/Footer";
 import { Header } from "../../shared/components/Header";
 import { Main } from "./styled";
+
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
 
 export const Dashboard = () => {
   return (
     <>
       <Header />
-      <Main></Main>
+      <Main>
+        <Grid container rowSpacing={3} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+          <Grid item xs={2}>
+            <CardOrcamento />
+          </Grid>
+        </Grid>
+      </Main>
       <Footer />
     </>
   );
--- src/pages/Dashboard/styled.js
+++ src/pages/Dashboard/styled.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 export const Main = styled.main`
   width: 100vw;
   height: calc(100vh - 7rem);
-  display: flex;
-  justify-content: center;
-  align-items: center;
   background-color: green;
+  display: grid;
+  padding: 2.5rem;
+  /* grid-gap: 2rem; */
+  grid-template-columns: repeat(auto-fill, minmax(auto, 1fr));
 `;
+
+
