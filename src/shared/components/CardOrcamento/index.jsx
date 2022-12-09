a0c1b9857f8b3eb7d60a949925c14bf6ebe105d3
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-12-08 22:39:19 -0400

    :lipstick: feat: Estilizaçao CSS Card e Grid

--- src/shared/components/CardOrcamento/index.jsx
+++ src/shared/components/CardOrcamento/index.jsx
@@ -3,32 +3,48 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Chip } from "@mui/material";
+import { CardActionArea } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function CardOrcamento({ empresa }) {
   return (
     <Link to="/perfil" className="linkCard">
-      <Card sx={{ maxWidth: 270, height: 270 }}>
-        <CardActionArea sx={{ maxWidth: 270 }}>
+      <Card sx={{ width: 270, height: 270 }}>
+        <CardActionArea sx={{ width: 270 }}>
           <CardMedia
             component="img"
-            height="90"
+            height="110"
             image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
             alt="green iguana"
           />
-          <CardContent>
+
+          <CardContent
+            sx={{
+              height: 160,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
             <Typography gutterBottom variant="h6" component="div">
               {empresa}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                width: 270,
+                textAlign: "right",
+                paddingRight: 3,
+              }}
+            >
               Pedido:0045
             </Typography>
           </CardContent>
         </CardActionArea>
-        <Chip label="success" color="success" />
       </Card>
-      <Chip label="primary" color="primary" />
     </Link>
   );
 }
